5739ef534eeb4488d45f0d48d2a406b248eb3a83
Daniel H <naduran@live.se>
2021-06-15 14:08:17 +0200

    add lines

--- Grades/GradeBook.cs
+++ Grades/GradeBook.cs
@@ -8,12 +8,17 @@ namespace Grades
 {
     class GradeBook
     {
+
+        public GradeBook()
+        {
+            grades = new List<float>();
+        }
         public void AddGrade(float grade)
         {
             grades.Add(grade);
         }
 
-        List<float> grades = new();
+        List<float> grades;
     }
 }
  
